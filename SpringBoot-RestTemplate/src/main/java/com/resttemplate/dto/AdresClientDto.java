d265a65d35aa72a037e9dd38c82752acf5c841ff
İbrahim BOLAT <bolat6606@hotmail.com>
2021-06-25 01:19:54 +0300

    commit

--- SpringBoot-RestTemplate/src/main/java/com/resttemplate/dto/AdresClientDto.java
+++ SpringBoot-RestTemplate/src/main/java/com/resttemplate/dto/AdresClientDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
---
+++ SpringBoot-RestTemplate/src/main/java/com/resttemplate/utils/ModelMapperUtil.java
@@ -0,0 +1,17 @@
+package com.resttemplate.utils;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModelMapperUtil {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        ModelMapper modelMapper=new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
